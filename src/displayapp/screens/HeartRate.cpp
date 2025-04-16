ab92d4b3fe47d9afd77d7d15d19764ef51195cd1
Maximilien Vanstaevel <91668112+rurueuh@users.noreply.github.com>
2025-04-16 19:00:39 +0900

    Update HeartRate.cpp

--- src/displayapp/screens/HeartRate.cpp
+++ src/displayapp/screens/HeartRate.cpp
@@ -45,7 +45,7 @@ HeartRate::HeartRate(Controllers::HeartRateController& heartRateController, Syst
   lv_obj_align(label_hr, nullptr, LV_ALIGN_CENTER, 0, -40);
 
   label_bpm = lv_label_create(lv_scr_act(), nullptr);
-  lv_label_set_text_static(label_bpm, "Heart rate BPM");
+  lv_label_set_text_static(label_bpm, "Heart rate BPM LOVE");
   lv_obj_align(label_bpm, label_hr, LV_ALIGN_OUT_TOP_MID, 0, -20);
 
   label_status = lv_label_create(lv_scr_act(), nullptr);
